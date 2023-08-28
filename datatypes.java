e40a05065dfd1970360c331e42c335390e6a997e
Pooja bharathi. P <pooja.2002@srit.org>
2023-08-28 13:02:15 +0530

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,15 @@
+class datatypes
+{
+    public static void main(String args[])
+    {
+        float marks = 5.0f;
+        byte by = 127;
+        short sh = 557;
+        long l = 5854l;
+        float f = 5.8f;
+        char c = 'n';
+        boolean b = false;
+
+        System.out.println(b);
+    }
+}
---
+++ variables.java
@@ -0,0 +1,9 @@
+class variables
+{
+    public static void main(String arsg[])
+    {
+        int num1 = 5;
+        int num2 = 3;
+        System.out.println(num1+num2);
+    }
+}
